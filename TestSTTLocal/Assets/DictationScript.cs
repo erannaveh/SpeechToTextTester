38712d72623ec4236639d81fa1bbbd25c47679c9
Eran Naveh <eran@umail.ucsb.edu>
2019-11-13 18:16:12 -0800

    Local STT

---
+++ TestSTTLocal/Assets/DictationScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+//1. Edit->Project Settings->Player
+//2. Windows Store Tab
+//3. Publishing Settings->Capabilities: check the "Microphone" capability
+
+public class DictationScript : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_Hypotheses;
+
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    protected void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses.text += text + Env;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (cause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", cause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
+ 
+    }
+
+   protected void Stop()
+    { 
+        m_DictationRecognizer.Stop();
+        m_DictationRecognizer.Dispose();
+    }
+}
---
+++ TestSTTLocal/Assets/KeyWordRecognizerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+
+public class KeyWordRecognizerScript : DictationScript
+{ 
+    public KeywordRecognizer keywordRecognizer;
+    public Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Start is called before the first frame update
+    //Create keywords for keyword recognizer
+
+
+    new void Start()
+    {
+        keywords.Add("activate", () =>
+        {
+            base.Start();
+        });
+
+        keywords.Add("stop", () =>
+        {
+            base.Stop();
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+    }
+
+    public void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+         System.Action keywordAction;
+    // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+           keywordAction.Invoke();}
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        KeywordRecognizer_OnPhraseRecognized()
+    }
+}
